7914c269a2f03bbc5df847c664b3824344c899b2
letrongtai902 <tai.le@gumiviet.com>
2022-03-14 20:47:05 +0700

    sorry student

--- Lession3/lists.py
+++ Lession3/lists.py
@@ -1,2 +1,12 @@
 if __name__ == '__main__':
     N = int(input())
+    #cho mảng trả về là mảng rỗng
+    result = []
+    for i in range(N):
+        # chuyển input đầu vào thành list split khoảng trắng
+        # lis có thể có 1 2 3 phần tử, phần tử đầu tiên luôn là lệnh
+        arr = input().split()
+        if arr[0] == "insert":
+            result.insert(int(arr[1]), int(arr[2]))
+        elif arr[0] == "print":
+            print(result)       
