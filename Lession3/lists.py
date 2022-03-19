a247979aba5259144397f6c12e2edab2b17ac524
Nagomichan <nttggiang98@gmail.com>
2022-03-19 20:54:43 +0700

    bai tap lists

--- Lession3/lists.py
+++ Lession3/lists.py
@@ -8,5 +8,15 @@ if __name__ == '__main__':
         arr = input().split()
         if arr[0] == "insert":
             result.insert(int(arr[1]), int(arr[2]))
+        elif arr[0] == "remove":
+            result.remove(int(arr[1]))
+        elif arr[0] == "append":
+            result.append(int(arr[1]))
+        elif arr [0] == "sort":
+            result.sort()
+        elif arr [0] == "pop":
+            result.pop()
+        elif arr [0] == "reverse":
+            result.reverse()
         elif arr[0] == "print":
             print(result)       
